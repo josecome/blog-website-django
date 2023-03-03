c5b3f504df8716631efc76358d3b37fca805925d
Jose Jaime Come <josejaimecome@gmail.com>
2023-03-03 17:30:10 +0200

    Link option for contents detail added

--- contents/models.py
+++ contents/models.py
@@ -11,6 +11,7 @@ class Content(models.Model):
     author_updated = models.CharField(max_length=40)      
     date_created = models.DateField()
     date_updated = models.DateField()
+    lnk = models.CharField(max_length=100, default='lnk')  
 
     class Meta:  
         db_table = "contents"
