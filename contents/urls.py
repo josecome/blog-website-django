61db0effd0d615c9201a27a62ff6487912dadfac
Jose Jaime Come <josejaimecome@gmail.com>
2023-06-03 21:30:03 +0200

    Minor changes made

--- contents/urls.py
+++ contents/urls.py
@@ -4,6 +4,6 @@ from contents import views
 
 urlpatterns = [
     path('', views.Contents, name="contents"),   
-    path('create_content/', views.Create_Content, name="create_content"),
+    path('create_content', views.Create_Content, name="create_content"),
     path('create_content_add/', views.Create_Content_Insert, name="create_content_add"),
 ]
