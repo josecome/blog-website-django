a41f1a45d60373ddebb3605349085b8dd889a8ba
Jose Jaime Come <josejaimecome@gmail.com>
2023-09-02 21:21:09 +0200

    Minor changes made

--- blog/views.py
+++ blog/views.py
@@ -39,6 +39,7 @@ def loginPage(request):
         if request.method == 'POST':
             username = request.POST["username"]
             password = request.POST["password"]
+            # admin, password
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
